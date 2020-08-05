264645291c368dd7623f61c6d63ff95e16e1b009
Luis Dejesus <luisdejesus@Luiss-MacBook-Air.local>
2020-08-05 16:13:01 -0400

    Log in, Sign up, Logo

--- frontend/src/components/auth/SignUp.js
+++ frontend/src/components/auth/SignUp.js
@@ -22,6 +22,30 @@ class SignUp extends Component {
                     <input name="password" type="password" onChange={this.handleChange} />
                     <input type="submit" value="Sign Up"/>
                 </form>
+
+
+
+                <div id="login-box">
+  <div class="left">
+    <h1>Sign up</h1>
+    
+    <input type="text" name="username" placeholder="Username" />
+    <input type="text" name="email" placeholder="E-mail" />
+    <input type="password" name="password" placeholder="Password" />
+    <input type="password" name="password2" placeholder="Retype password" />
+    
+    <input type="submit" name="signup_submit" value="Sign me up" />
+  </div>
+  
+  <div class="right">
+    <span class="loginwith">Sign in with<br />social network</span>
+    
+    <button class="social-signin facebook">Log in with facebook</button>
+    <button class="social-signin twitter">Log in with Twitter</button>
+    <button class="social-signin google">Log in with Google+</button>
+  </div>
+  <div class="or">OR</div>
+</div>
             </Fragment>
         );
     }
